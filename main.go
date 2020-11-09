56d1b94a7833acfdbcbabc4c7e090bb3e578e4f4
BRO3886 <siddverma1999@gmail.com>
2020-11-09 15:19:32 +0530

    write method to generate config.json

--- main.go
+++ main.go
@@ -16,103 +16,36 @@ limitations under the License.
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
+	"io/ioutil"
 	"os"
 
 	"github.com/BRO3886/google-tasks-cli/cmd"
-
-	"golang.org/x/net/context"
-	"golang.org/x/oauth2"
 )
 
-// Retrieve a token, saves the token, then returns the generated client.
-func getClient(config *oauth2.Config) *http.Client {
-	// The file token.json stores the user's access and refresh tokens, and is
-	// created automatically when the authorization flow completes for the first
-	// time.
-	tokFile := "token.json"
-	tok, err := tokenFromFile(tokFile)
-	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(tokFile, tok)
-	}
-	return config.Client(context.Background(), tok)
-}
-
-// Request a token from the web, then returns the retrieved token.
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the "+
-		"authorization code: \n%v\n", authURL)
-
-	var authCode string
-	if _, err := fmt.Scan(&authCode); err != nil {
-		log.Fatalf("Unable to read authorization code: %v", err)
-	}
-
-	tok, err := config.Exchange(context.TODO(), authCode)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web: %v", err)
-	}
-	return tok
-}
-
-// Retrieves a token from a local file.
-func tokenFromFile(file string) (*oauth2.Token, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	tok := &oauth2.Token{}
-	err = json.NewDecoder(f).Decode(tok)
-	return tok, err
-}
-
-// Saves a token to a file path.
-func saveToken(path string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to: %s\n", path)
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
-	}
-	defer f.Close()
-	json.NewEncoder(f).Encode(token)
+func generateConfig() {
+	credString := `
+	{
+		"installed": {
+			"client_id": "415973160530-1onpd10rt7vl0dc79sh0hf8qb7ilc0bo.apps.googleusercontent.com",
+			"project_id": "tasks-cli-tool-1604690538991",
+			"auth_uri": "https://accounts.google.com/o/oauth2/auth",
+			"token_uri": "https://oauth2.googleapis.com/token",
+			"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+			"client_secret": "s-QOvdsBvXh7vgIGJSgojFa7",
+			"redirect_uris": [
+				"urn:ietf:wg:oauth:2.0:oob",
+				"http://localhost"
+			]
+		}
+	}`
+	mode := int(0666)
+	ioutil.WriteFile("config.json", []byte(credString), os.FileMode(mode))
 }
 
 func main() {
-	// b, err := ioutil.ReadFile("credentials.json")
-	// if err != nil {
-	// 	log.Fatalf("Unable to read client secret file: %v", err)
-	// }
-
-	// // If modifying these scopes, delete your previously saved token.json.
-	// config, err := google.ConfigFromJSON(b, tasks.TasksScope)
-	// if err != nil {
-	// 	log.Fatalf("Unable to parse client secret file to config: %v", err)
-	// }
-	// client := getClient(config)
-
-	// srv, err := tasks.New(client)
-	// if err != nil {
-	// 	log.Fatalf("Unable to retrieve tasks Client %v", err)
-	// }
-
-	// r, err := srv.Tasklists.List().MaxResults(10).Do()
-	// if err != nil {
-	// 	log.Fatalf("Unable to retrieve task lists. %v", err)
-	// }
-
-	// fmt.Println("Task Lists:")
-	// if len(r.Items) > 0 {
-	// 	for _, i := range r.Items {
-	// 		fmt.Printf("%s (%s)\n", i.Title, i.Id)
-	// 	}
-	// } else {
-	// 	fmt.Print("No task lists found.")
-	// }
+	_, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		generateConfig()
+	}
 	cmd.Execute()
 }
