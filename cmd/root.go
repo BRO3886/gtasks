d50f2ae16b9c40d1efb3b20f092f05b26204b34d
BRO3886 <39856034+BRO3886@users.noreply.github.com>
2021-06-22 16:05:33 +0530

    :memo: version bump

--- cmd/root.go
+++ cmd/root.go
@@ -16,7 +16,7 @@ var cfgFile string
 var rootCmd = &cobra.Command{
 	Use:     "gtasks",
 	Short:   "A CLI Tool for Google Tasks",
-	Version: "0.9.1",
+	Version: "0.9.2",
 	Long: `
 	A CLI Tool for managing your Google Tasks:
 	
